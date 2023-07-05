dfd2a0973fb78018025f350e3eee7567ecb85c69
Artur Fernandes Braga de Menezes <138509581+artur70152@users.noreply.github.com>
2023-07-05 11:22:31 -0300

    Update persistreducer.js

--- front/front/src/funcionalidades/persistreducer.js
+++ front/front/src/funcionalidades/persistreducer.js
@@ -1,10 +1,15 @@
 import storage from 'redux-persist/lib/storage'
 import {persistReducer} from 'redux-persist';
-export default reducers=>{
-    const persistedReducer=persistReducer({
-key:'donoc',
-storage,
-whitelist:['auth','user','auth.token'],
-    },reducers)
-    return persistedReducer
-}
+const createPersistedReducer = reducers => {
+    const persistedReducer = persistReducer(
+      {
+        key: 'donoc',
+        storage,
+        whitelist: ['auth', 'user', 'auth.token'],
+      },
+      reducers
+    );
+    return persistedReducer;
+  };
+  
+  export default createPersistedReducer;
